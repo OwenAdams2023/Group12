7ef662755b9f758390abd95d20a929ff0c2ec2b6
dmilsap <156827373+dmilsap@users.noreply.github.com>
2024-03-19 12:34:09 -0500

    Update urls.py
    Added checkout, order success, return request, and return request success urls

--- ecom/sneakpeek/urls.py
+++ ecom/sneakpeek/urls.py
@@ -12,4 +12,8 @@ urlpatterns = [
     path('product/<int:pk>', views.product, name='product'),
     path('category/<str:cat_name>', views.category, name='category'),
     path('category_summary/', views.category_summary, name='category_summary'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('order_success/', views.order_success, name='order_success'),
+    path('return_request/<int:order_id>/', views.return_request, name='return_request'),
+    path('return_request_success/', views.return_request_success, name='return_request_success'),
 ]
